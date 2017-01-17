aca3c1012512cceaf9b5157d529ce103a963e1c3
sandrine <sandrine@10.0.2.15>
2017-01-17 13:27:00 +0100

    test mainlogiciel

---
+++ src/GCLASS/MainLogiciel.java
@@ -0,0 +1,13 @@
+package GCLASS;
+
+public class MainLogiciel {
+
+	public static void main(String[] args) {
+
+		//VueAccueil fen = new VueAccueil();
+		//VueAdminAcc admin1 = new VueAdminAcc();
+		//VueAdminComptes admin2 = new VueAdminComptes();
+		//VueAdminPromos admin2 = new VueAdminPromos();
+	}
+
+}
